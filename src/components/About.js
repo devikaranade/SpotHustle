1041ef2a40feb34ec653424399b4063c5dd6a2dc
Devika <devika24march@.com>
2024-05-29 17:09:54 -0700

    ui changes

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,13 @@ const About = () => (
   <section id="about" className="about">
     <h2>About SpotHustle</h2>
     <p>
-      SpotHustle is an innovative application designed to revolutionize the way
-      parking spots are managed and utilized. SpotHustle offers a streamlined
-      platform where individuals can auction their available parking spots for
-      others to conveniently use.
+      SpotHustle is here to transform the parking experience with a touch of
+      innovation and convenience. Imagine a world where finding a parking spot
+      is no longer a hassle but an opportunity. SpotHustle provides a dynamic
+      platform where individuals can auction their available parking spots,
+      turning everyday spaces into valuable assets. Whether you're looking to
+      park or profit, SpotHustle seamlessly connects you to the perfect spot,
+      making parking as effortless as a tap on your screen.
     </p>
   </section>
 );
